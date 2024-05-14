b97fae0d3d4a774824bea6a1aeed4cdbd2e14597
Robert Wagner <rw@20sec.net>
2024-05-14 23:27:34 +0200

    form associated elements

---
+++ src/components/form-elements/form.js
@@ -0,0 +1,83 @@
+import Input from "./input.js";
+import Object from "./object.js";
+const template = document.createElement("template");
+
+template.innerHTML = /*html*/ `
+<link rel="stylesheet" href="${
+  import.meta.url
+}/../variables.css" type="text/css">
+<link rel="stylesheet" href="${
+  import.meta.url
+}/../bootstrap.min.css" type="text/css">
+<style>
+
+:host{
+    display:block;
+    padding: 1rem;
+    font-family: var(--bs-body-font-family);
+}
+form{
+
+}
+
+</style>
+
+<div class="container">
+<form>
+<button class="btn btn-primary mb-5">Speichern</button>
+<div class="invalid-feedback"></div>
+</form>
+</div>
+`;
+
+export default class Form extends HTMLElement {
+  // static formAssociated = true;
+  // static observedAttributes = ["value"];
+
+  constructor() {
+    // Inititialize custom component
+    super();
+    this.attachShadow({ mode: "open" });
+    // this.internals = this.attachInternals();
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.els = {
+      form: this.shadowRoot.querySelector("form"),
+      button: this.shadowRoot.querySelector("button"),
+      error: this.shadowRoot.querySelector(".invalid-feedback"),
+    };
+    console.log("form-els", this.els);
+    this.els.form.addEventListener("submit", (e) => this.save(e));
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // this.internals.setFormValue(newValue);
+  }
+
+  save(e) {
+    let data = new FormData(this.els.form);
+    console.log("save", data, this.els.form);
+    e.preventDefault();
+    return false;
+  }
+
+  set props(p) {
+    this._label = p.label;
+    this._name = p.name;
+    this._id = p.id;
+  }
+
+  render() {
+    let el = new Input();
+    el.props = { label: "E-Mail-Adresse", name: "email", id: "email" };
+    el.setAttribute("value", "horst@example.com");
+    this.els.form.appendChild(el);
+    let obj = new Object();
+    this.els.form.appendChild(obj);
+  }
+}
+
+customElements.define("pi-form", Form);
---
+++ src/components/form-elements/input.js
@@ -0,0 +1,72 @@
+const template = document.createElement("template");
+
+template.innerHTML = /*html*/ `
+<link rel="stylesheet" href="${
+  import.meta.url
+}/../bootstrap.min.css" type="text/css">
+<div class="fgroup">
+    <label class="form-label"></label>
+    <input type="text" class="form-control">
+    <div class="invalid-feedback"></div>
+</div>
+`;
+
+export default class Input extends HTMLElement {
+  static formAssociated = true;
+  static observedAttributes = ["value"];
+
+  constructor() {
+    // Inititialize custom component
+    super();
+    this.attachShadow({ mode: "open" });
+    this.internals = this.attachInternals();
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.els = {
+      label: this.shadowRoot.querySelector("label"),
+      field: this.shadowRoot.querySelector("input"),
+      error: this.shadowRoot.querySelector(".invalid-feedback"),
+    };
+    this.els.field.addEventListener("input", (e) => {
+      console.log("++ input ev.input", this._name, e.target.value);
+      this.internals.setFormValue(e.target.value);
+    });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log("++ new value", newValue, this);
+    this.internals.setFormValue(newValue);
+    this.render();
+  }
+
+  set value(v) {
+    this.setAttribute("value", v);
+  }
+
+  get value() {
+    //return "noidea";
+    console.log("++ internals get value", this.internals);
+    return this.els.field.value;
+  }
+
+  set props(p) {
+    this._label = p.label;
+    this._name = p.name;
+    this.setAttribute("name", p.name);
+    this._id = p.id;
+    this.setAttribute("id", p.id);
+  }
+
+  render() {
+    this.els.field.value = this.getAttribute("value");
+    this.els.label.innerHTML = this._label;
+    this.els.label.setAttribute("for", this._id);
+    this.els.field.setAttribute("id", this._id);
+    this.els.field.setAttribute("name", this._name);
+  }
+}
+
+customElements.define("form-input", Input);
