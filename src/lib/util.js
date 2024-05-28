bac0f3a3eaeb6d5976fa1ee6cd5b54fdb847e89a
rw <rw@20sec.net>
2024-05-28 13:14:17 +0200

    hashID functions

--- src/lib/util.js
+++ src/lib/util.js
@@ -14,3 +14,15 @@ export function once(fn, context) {
     return result;
   };
 }
+
+export const hashID = (size) => {
+  const MASK = 0x3d;
+  const LETTERS = "abcdefghijklmnopqrstuvwxyz";
+  const NUMBERS = "1234567890";
+  const charset = `${NUMBERS}${LETTERS}${LETTERS.toUpperCase()}_-`.split("");
+
+  const bytes = new Uint8Array(size);
+  crypto.getRandomValues(bytes);
+
+  return bytes.reduce((acc, byte) => `${acc}${charset[byte & MASK]}`, "");
+};
