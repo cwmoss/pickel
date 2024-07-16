1287e593f4d14f2d3eb29b5a70abf573e3a3028f
Robert Wagner <rw@20sec.net>
2024-07-16 20:46:37 +0200

    geo point

--- src/form-elements/geo.js
+++ src/form-elements/geo.js
@@ -1,4 +1,4 @@
-import { LitElement, css, html } from "./../../vendor/lit-core.min.js";
+import { LitElement, css, html, render } from "./../../vendor/lit-core.min.js";
 import Input from "./input.js";
 import Face from "./face.js";
 import { get_document, api } from "./_helper.js";
@@ -21,40 +21,101 @@ let style = css`
     display: inline-block;
   }
 `;
+// berlin
+let geo_default = { lat: 52.518611, lng: 13.408333 };
 
 export default class Geo extends Face {
-  static properties = { ...Face.properties, prefix: {}, suffix: {} };
+  static properties = {
+    ...Face.properties,
+    prefix: {},
+    suffix: {},
+    marker: { type: Object },
+  };
   static styles = [style];
   connectedCallback() {
     super.connectedCallback();
     setTimeout(() => this.init(), 100);
   }
 
+  add_marker(latlong) {
+    if (this.marker) {
+      this.marker.setLatLng(latlong);
+      // this.marker.addTo(this.map)
+      this.value = latlong;
+    } else {
+      this.marker = L.marker(latlong, { draggable: true }).addTo(this.map);
+      this.value = latlong;
+      let content = document.createElement("div");
+      let btn = html`<pi-btn
+        flat
+        @click=${() => {
+          this.marker.remove();
+          this.marker = null;
+          this.value = null;
+        }}
+        >remove</pi-btn
+      >`;
+      render(btn, content);
+      /*
+      let btn = document.createElement("pi-btn");
+      btn.flat = true;
+      btn.innerHTML = "remove";
+      btn.addEventListener("click", (e) => {
+        console.log("$$ popup click", e);
+        this.marker.remove();
+        this.marker = null;
+        this.value = null;
+      });
+      */
+      let popup = this.marker.bindPopup(content).getPopup();
+      // popup.addEventlistener("click", () => console.log("button clicked?"));
+      this.marker.on("move", (e) => {
+        console.log("move", e);
+        this.value = e.latlng;
+      });
+    }
+  }
+
   init() {
     this.el = this.shadowRoot.querySelector("div");
     this.map = L.map(this.el).setView(this.latlong, 13); // [51.505, -0.09]
+    L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.9.4/dist/images/";
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.map);
+
+    if (this.value.lat) {
+      this.add_marker(this.value);
+    }
+
+    this.map.on("click", (e) => {
+      console.log("clicked to map", e);
+      this.add_marker(e.latlng);
+    });
   }
   get latlong() {
-    let latlong = this.value;
+    let latlong = geo_default; // this.value;
+    if (this.value.lat) {
+      latlong = this.value;
+    }
     console.log("++ latlong ++", latlong);
+    return latlong;
     return L.latLng(latlong.lat, latlong.lng);
   }
   get_updated_data() {
+    return this.value;
     // TODO: warum wird das frühzeitig aufgerufen?
     return {};
   }
 
   update_input(e) {
     console.log("+++ update", hashID(5), e, e.target.value);
-    this.value.current = e.target.value;
+    // this.value.current = e.target.value;
   }
   render() {
-    console.log("render text");
+    // console.log("render MAP", this.value);
     return html`<link
         rel="stylesheet"
         href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
