258fe9f5e57153b3b11d2f948533e092cc71608c
rw <rw@20sec.net>
2026-03-11 01:04:53 +0100

    bubble tooltip

--- tiptap/bubble-menu.js
+++ tiptap/bubble-menu.js
@@ -8,7 +8,8 @@ import { computePosition, flip, shift } from "../vendor/floating-ui-bundle.js";
 
 function new_item(command, title) {
     return {
-        command, title
+        command,
+        title,
     };
 }
 
@@ -21,75 +22,88 @@ export default class BubbleMenu extends LitElement {
     static styles = [
         // cssvars,
         css`
-      :host {
-        // display: block;
-        position: relative;
-        --menu-pos-top: 0;
-        --menu-pos-left: 0;
-      }
-      #rel {
-        // position: relative;
-      }
+            :host {
+                // display: block;
+                position: relative;
+                --menu-pos-top: 0;
+                --menu-pos-left: 0;
+            }
+            #rel {
+                // position: relative;
+            }
 
-      #toggle {
-        /* anchor-name: --menu; */
-      }
+            #toggle {
+                /* anchor-name: --menu; */
+            }
 
-      #menu {
-        position: absolute;
-        inline-size: max-content;
-        // display: none;
-        border: none;
-        background-color: white;
-        --shadow-linie-color: rgba(114, 120, 146, 0.3);
-        --shadow-schatten-color: rgba(114, 120, 146, 0.1);
-        --shadow-halbschatten-color: rgba(114, 120, 146, 0.07);
-        --shadow-umgebung-color: rgba(114, 120, 146, 0.06);
-        border-radius: 0.375rem;
-        box-shadow: 0 0 0 0.03125rem var(--shadow-linie-color),
-          0 0.4375rem 0.5rem -0.25rem var(--shadow-schatten-color),
-          0 0.75rem 1.0625rem 0.125rem var(--shadow-halbschatten-color),
-          0 0.3125rem 1.375rem 0.25rem var(--shadow-umgebung-color);
-      }
-      :popover-open {
-        width: max-content;
-        /* height: 200px; */
-        position: absolute;
-        inset: unset;
-        top: var(--menu-pos-top);
-        left: var(--menu-pos-left);
-        margin: 0;
-      }
-      .listcontainer,
-      .subcontainer {
-        display: flex;
-        flex-direction: row;
-      }
+            #menu {
+                position: absolute;
+                inline-size: max-content;
+                // display: none;
+                border: none;
+                background-color: white;
+                --shadow-linie-color: rgba(114, 120, 146, 0.3);
+                --shadow-schatten-color: rgba(114, 120, 146, 0.1);
+                --shadow-halbschatten-color: rgba(114, 120, 146, 0.07);
+                --shadow-umgebung-color: rgba(114, 120, 146, 0.06);
+                border-radius: 0.75rem;
+                box-shadow:
+                    0 0 0 0.03125rem var(--shadow-linie-color),
+                    0 0.4375rem 0.5rem -0.25rem var(--shadow-schatten-color),
+                    0 0.75rem 1.0625rem 0.125rem
+                        var(--shadow-halbschatten-color),
+                    0 0.3125rem 1.375rem 0.25rem var(--shadow-umgebung-color);
+            }
+            :popover-open {
+                width: max-content;
+                /* height: 200px; */
+                position: absolute;
+                inset: unset;
+                top: var(--menu-pos-top);
+                left: var(--menu-pos-left);
+                margin: 0;
+            }
+            .listcontainer,
+            .subcontainer {
+                display: flex;
+                flex-direction: row;
+            }
 
-      a {
-        flex: 1;
-        text-decoration: none;
-        outline: none;
-        line-height: 3;
-        color: black;
-        padding: 0 1rem;
-      }
+            button {
+                flex: 1;
+                text-decoration: none;
+                outline: none;
+                line-height: 3;
+                color: black;
+                padding: 0 0.5rem;
+                border-radius: 0.75rem;
+                background: #fff;
+                border: transparent;
+            }
 
-      a:link,
-      a:visited {
-        color: black;
-      }
+            button:hover,
+            button:focus {
+                background: var(--color-accent);
+            }
 
-      a:hover,
-      a:focus {
-        background: var(--color-accent);
-      }
+            button:active {
+                background: #999;
+                /* color: white; */
+            }
+            .tooltip[popover] {
+                background: #007bff;
+                color: #fff;
+            }
 
-      a:active {
-        background: #999;
-        /* color: white; */
-      }
-    `,
+            .tooltip[popover] {
+                left: calc(anchor(right) + 2px);
+                top: anchor(center);
+                transform: translateY(-50%);
+            }
+            button:hover + .tooltip {
+                visibility: visible;
+            }
+        `,
     ];
 
     connectedCallback() {
@@ -114,13 +128,20 @@ export default class BubbleMenu extends LitElement {
         });
         this.dispatchEvent(evt);
     }
-
+    exec(e) {
+        console.log("++exit", e.target);
+    }
+    enter(e) {
+        let popover = e.target.querySelector("[popover]");
+        console.log("++enter", e.target, popover);
+        if (popover) popover.showPopover();
+    }
+    leave(e) {
+        let popover = e.target.querySelector("[popover]");
+        if (popover) popover.hidePopover();
+    }
     show(trigger) {
-        console.log(
-            "++ show ++",
-            trigger,
-            trigger.getBoundingClientRect()
-        );
+        console.log("++ show ++", trigger, trigger.getBoundingClientRect());
         let t = trigger;
 
         let v = {
@@ -160,15 +181,32 @@ export default class BubbleMenu extends LitElement {
         });
     }
     active = false;
-    toggle() { }
+    toggle() {}
     render() {
         return html`<div id="menu" popover @click=${this.select}>
-      <nav class="listcontainer">
-        ${this.items.map((it) => {
-            return html`<a href=${it.command}>${it.title}</a>`;
-        })}
-      </nav>
-    </div>`;
+            <nav class="listcontainer">
+                ${this.items.map((it) => {
+                    return html`<button
+                        id=${it.command}
+                        popovertarget="${it.command}_info"
+                        @click=${this.exec}
+                        @mouseenter=${this.enter}
+                        @mouseleave=${this.leave}
+                    >
+                        ${it.title}
+                        <div
+                            role="tooltip
+                            id="${it.command}_info"
+                            popover="manual"
+                            anchor="${it.command}"
+                            class="tooltip left"
+                        >
+                            Lorem ipsum dolor sit amet
+                        </div>
+                    </button>`;
+                })}
+            </nav>
+        </div>`;
     }
 }
 
